9f4fa369b02c9dae8e3c060141217d43bb72744b
krehberg12 <krehberg12@gmail.com>
2020-02-26 16:59:57 -0500

    add extend registration functionality

--- routes/guestParking.js
+++ routes/guestParking.js
@@ -63,18 +63,86 @@ router.get("/parking/:id", async function(req, res){
         let foundProperty = await db.Property.findById(req.params.id);
         if(foundProperty){
             res.render("show", {property: foundProperty});
-        }
+        } else {
+            res.redirect('back');
+        } 
     } catch(err){
         console.log(err);
         res.redirect("back")
     }   
 });
 
+//extend registration route
+router.get("/parking/:id/extend", async function(req, res){
+    try{
+        let property = await db.Property.findById(req.params.id);
+        if(property){
+            res.render('extend', {property: property});
+        } else {
+            res.redirect('back');
+        } 
+    } catch(err){
+        console.log(err);
+    }  
+});
+
+//extend registration post route
+router.post("/parking/:id/extend", async function(req, res){
+    try{
+        let tag = req.body.tag.toUpperCase();
+        let guest = await db.Guest.findOne({tag: tag});
+        if(guest){
+            res.redirect('/parking/' + req.params.id + "/extend/" + guest._id);
+        } else {
+            res.redirect('back');
+        }    
+    } catch(err){
+        console.log(err);
+    }  
+});
+
+//show extend registration form
+router.get("/parking/:id/extend/:id", async function(req, res){
+    try{ 
+        let guest = await db.Guest.findById(req.params.id);
+        if(guest){
+            res.render("extendRegistration", {guest: guest});
+        } else {
+            res.redirect('back');
+        } 
+    } catch(err){
+        console.log(err);
+    }  
+});
+
+//show extend registration form
+router.post("/parking/:id/extend/:id", async function(req, res){
+    try{ 
+        let guest = await db.Guest.findById(req.params.id);
+        if(guest){
+            guest.time = moment(req.body.extendedTime).format('YYYY-MM-DD HH:mm');
+            guest.endDate = moment(guest.time).format("dddd - MMMM Do, YYYY");
+            guest.endTime = moment(guest.time).format('h:mm a');
+            await guest.save();
+            res.render("success", {guest: guest});
+        } else {
+            res.redirect('back');
+        }    
+    } catch(err){
+        console.log(err);
+    }  
+});
+
+
 //show property registration options
 router.get("/parking/:id/register", async function(req, res){
     try{
         let foundProperty = await db.Property.findById(req.params.id);
-        res.render("register", {property: foundProperty});
+        if(foundProperty){
+            res.render("register", {property: foundProperty});
+        } else {
+            res.redirect('back');
+        } 
     } catch(err){
         console.log(err);
     }
@@ -84,42 +152,41 @@ router.get("/parking/:id/register", async function(req, res){
 router.post("/parking/:id/register", async function(req, res){
     try {
         let property = await db.Property.findById(req.params.id);
-        let guest = await db.Guest.create(req.body.guest);
-        guest.property = property.name;
-        guest.permit = Math.floor(100000 + Math.random() * 900000);
-        await guest.save();
-        await property.guests.push(guest);
-        await property.save();
-        console.log(property);
-        res.redirect("/parking/" + req.params.id + "/register/" + guest._id);
+        if(property){
+            let guest = await db.Guest.create(req.body.guest);
+            guest.tag = guest.tag.toUpperCase();
+            guest.property = property.name;
+            guest.permit = Math.floor(100000 + Math.random() * 900000);
+            await guest.save();
+            await property.guests.push(guest);
+            await property.save();
+            console.log(property);
+            res.redirect("/parking/" + req.params.id + "/register/" + guest._id);
+        } else {
+            res.redirect('back');
+        } 
     } catch(err){
         console.log(err);
     }
 });
 
-// //extend registration route
-// router.get("/parking/:id/extend", async function(req, res){
-//     try{
-//         let property = await db.Property.findById(req.params.id);
-//         let guest = await db.Guest.find({_id: property.guests});
-        
-//     } catch(err){
-//         console.log(err);
-//     }  
-// });
-
 //successful registration route
 router.get("/parking/:id/register/:id", async function(req, res){
         try {
             let guest = await db.Guest.findById(req.params.id);
-            guest.time = moment(guest.time).format('YYYY-MM-DD HH:mm');
-            guest.startDate = moment.tz('America/New_York').format('dddd - MMMM Do, YYYY');
-            guest.endDate = moment(guest.time).format("dddd - MMMM Do, YYYY");
-            guest.startTime = moment.tz('America/New_York').format('h:mm a');
-            guest.endTime = moment(guest.time).format('h:mm a');
-            guest.active = true;
-            await guest.save();   
-            res.render("success", {guest: guest});
+            let property = await db.Property.findOne({name: guest.property});
+            if(guest){
+                guest.time = moment(guest.time).format('YYYY-MM-DD HH:mm');
+                guest.startDate = moment.tz('America/New_York').format('dddd - MMMM Do, YYYY');
+                guest.endDate = moment(guest.time).format("dddd - MMMM Do, YYYY");
+                guest.startTime = moment.tz('America/New_York').format('h:mm a');
+                guest.endTime = moment(guest.time).format('h:mm a');
+                guest.active = true;
+                await guest.save();
+                res.render("success", {guest: guest, property: property});
+            } else {
+                res.redirect('back');
+            }    
         } catch(err){
             console.log(err);
         }
