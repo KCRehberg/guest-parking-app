b0a875e32cd0cd55fcfe534dc84a25e3e4b8cf5b
krehberg12 <krehberg12@gmail.com>
2020-02-25 19:22:06 -0500

    timezone correction

--- routes/guestParking.js
+++ routes/guestParking.js
@@ -112,7 +112,7 @@ router.post("/parking/:id/register", async function(req, res){
 router.get("/parking/:id/register/:id", async function(req, res){
         try {
             let guest = await db.Guest.findById(req.params.id);
-            guest.time = moment(guest.time).tz('America/New_York').format("YYYY-MM-DD HH:mm");
+            guest.time = moment(guest.time).tz('America/New_York');
             guest.startDate = moment.tz('America/New_York').format('dddd - MMMM Do, YYYY');
             guest.endDate = moment(guest.time).format("dddd - MMMM Do, YYYY");
             guest.startTime = moment.tz('America/New_York').format('h:mm a');
